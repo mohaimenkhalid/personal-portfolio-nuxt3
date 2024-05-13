2bd636fb788abe75fb6f9ac8ce93deff2bf34d0d
Mohaimen <mohaimen@Mohaimens-MacBook-Air.local>
2024-05-13 06:44:48 +0600

    design added for contact information

--- components/contact.vue
+++ components/contact.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { PhoneForwarded, Mail, BookUser } from 'lucide-vue-next';
+import {PhoneForwarded, Mail, BookUser} from 'lucide-vue-next';
 </script>
 <template>
   <div class="contact-section">
@@ -35,16 +35,34 @@ import { PhoneForwarded, Mail, BookUser } from 'lucide-vue-next';
           </div>
         </div>
         <div class="w-full md:w-1/2">
-          <div class="contact-information ml-16 md:ml-20">
-            <div>
-              <PhoneForwarded />
-              phone</div>
-            <div>
-              <Mail />
-              Email</div>
-            <div>
-              <BookUser />
-              Address</div>
+          <div class="contact-information ml-16 md:ml-20 flex flex-col gap-y-8">
+            <div class="flex items-center gap-x-4">
+              <div class="contact-icon-box">
+                <PhoneForwarded class="contact-icon"/>
+              </div>
+              <div class="">
+                <div>phone</div>
+                <a class="text-xl" href="tel:+8801798659099">+8801798659099</a>
+              </div>
+            </div>
+            <div class="flex items-center gap-x-4">
+              <div class="contact-icon-box">
+                <Mail class="contact-icon"/>
+              </div>
+              <div>
+                <div>Email</div>
+                <a href="mailto:mohaimen707@gmail.com" class="text-xl">mohaimen707@gmail.com</a>
+              </div>
+            </div>
+            <div class="flex items-center gap-x-4">
+              <div class="contact-icon-box">
+                <BookUser class="contact-icon"/>
+              </div>
+              <div>
+                <div>Address</div>
+                <div class="text-xl">Dhaka, Bangladesh</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -53,39 +71,52 @@ import { PhoneForwarded, Mail, BookUser } from 'lucide-vue-next';
 </template>
 
 <style scoped>
-  .contact-section {
-    background: var(--tj-theme-accent-1);
-    position: relative;
-    overflow: hidden;
-  }
+.contact-section {
+  background: var(--tj-theme-accent-1);
+  position: relative;
+  overflow: hidden;
+}
 
-  .contact-form {
-    background: var(--tj-theme-accent-2);
-  }
+.contact-form {
+  background: var(--tj-theme-accent-2);
+}
 
-  .contact-form input, textarea {
-    display: block;
-    width: 100%;
-    background: var(--tj-black-2);
-    border: 1px solid var(--tj-grey-4);
-    font-size: 16px;
-    line-height: 1;
-    color: var(--tj-white);
-    padding: 14px 20px;
-    border-radius: 8px;
-    transition: all 0.3s 0s ease-out;
-    outline: none;
-    height: auto;
-    margin-bottom: 5px;
-  }
+.contact-form input, textarea {
+  display: block;
+  width: 100%;
+  background: var(--tj-black-2);
+  border: 1px solid var(--tj-grey-4);
+  font-size: 16px;
+  line-height: 1;
+  color: var(--tj-white);
+  padding: 14px 20px;
+  border-radius: 8px;
+  transition: all 0.3s 0s ease-out;
+  outline: none;
+  height: auto;
+  margin-bottom: 5px;
+}
 
-  .contact-form textarea {
-    min-height: 150px;
-  }
+.contact-form textarea {
+  min-height: 150px;
+}
 
-  .contact-information {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
+.contact-information {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.contact-icon-box {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background: -o-linear-gradient(289deg, var(--tj-theme-secondary) 0%, var(--tj-theme-primary) 100%);
+  background: linear-gradient(161deg, var(--tj-theme-secondary) 0%, var(--tj-theme-primary) 100%);
+  text-align: center;
+  line-height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
