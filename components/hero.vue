d9a94fb553399462a7cdd6883cd05fac8e7590ab
mohaimen <mohaimen707@gmail.com>
2024-05-14 22:58:51 +0600

    update primary color, font family, hero section design

--- components/hero.vue
+++ components/hero.vue
@@ -6,17 +6,34 @@ import { Download, Linkedin, Github } from 'lucide-vue-next';
   <section class="hero-section">
     <div class="container">
       <div class="w-full flex">
-        <div class="w-full md:w-1/2">
-          <span class="hero-sub-title">I am Mohaimen</span>
-          <h1 class="hero-title bg-clip text-[32px] md:text-[65px]">Software Engineer <br /> <span>(Frontend)</span></h1>
+        <div class="w-full md:w-2/3">
+          <span class="hero-sub-title">Welcome to my site.</span>
+          <h1 class="heading-hero ">I'm <strong>Abdul Mohaimen</strong>, a Frontend developer.</h1>
+
+
+<!--          <h1 class="hero-title bg-clip text-[32px] md:text-[65px]">Abdul Mohaimen</h1>-->
+<!--          <h1 class="hero-title bg-clip text-[32px] md:text-[30px]">Sr. Frontend Engineer</h1>-->
           <div class="w-full block md:hidden my-6">
             <div class="hero-image">
               <img class="!w-[250px] !h-[300px]" src="../assets/image/mohaimen_desk.webp"  alt="mohaimen-img" />
             </div>
           </div>
-          <p class="hero-lead">
-            I break down complex user experience
-            problems to create Integrity focussed solutions that connect billions of people
+          <p class="">
+            As a seasoned Software Engineer with over 7 years of experience 🚀,
+            I specialize in designing and implementing highly scalable API-based
+            solutions with a focus on improving performance. My biggest achievements
+            include creating efficient systems that have significantly improved business
+            operations for various clients.
+
+
+            With <strong>[4+ years]</strong> of experience in the industry, I've honed my skills in
+            crafting elegant frontend architectures that blend modern technology
+            with best practices in user experience design.
+          </p>
+          <p class="mt-4">
+            As a <strong>Lead Frontend Developer</strong> in @<strong>JATRI</strong>, I thrive on mentoring and leading development teams.
+            I've consistently upheld and strengthened my abilities in communication and collaboration,
+            cultivating fruitful connections with designers, frontend developers, backend developers, and stakeholders.
           </p>
           <div class="flex mt-10 items-center gap-5 flex-col md:flex-row">
             <a class="btn-download" href="">
@@ -33,7 +50,7 @@ import { Download, Linkedin, Github } from 'lucide-vue-next';
             </div>
           </div>
         </div>
-        <div class="w-full md:w-1/2 hidden md:block">
+        <div class="w-full md:w-1/3 hidden md:block">
           <div class="hero-image">
             <img src="../assets/image/mohaimen_desk.webp"  alt="mohaimen-img" />
           </div>
@@ -48,6 +65,26 @@ import { Download, Linkedin, Github } from 'lucide-vue-next';
 </template>
 
 <style scoped>
+
+.heading-hero {
+  margin-bottom: 1rem;
+  font-size: 4.5rem;
+  line-height: 1;
+  font-weight: 800;
+  letter-spacing: -.05em;
+}
+
+.heading-hero strong {
+  background-image: linear-gradient(to right, var(--tw-gradient-stops));
+  --tw-gradient-from: #06b6d4;
+  --tw-gradient-stops: var(--tw-gradient-from), var(--tw-gradient-to);
+  --tw-gradient-to: rgba(14,165,233,0);
+  --tw-gradient-to: #3b82f6;
+  -webkit-background-clip: text;
+  background-clip: text;
+  font-weight: 800;
+  color: transparent;
+}
 
 .hero-section {
   background: var(--tj-theme-accent-1);
@@ -93,8 +130,8 @@ import { Download, Linkedin, Github } from 'lucide-vue-next';
   -webkit-transition: 0.3s;
   -o-transition: 0.3s;
   transition: 0.5s;
-  mix-blend-mode: luminosity;
 }
+/*mix-blend-mode: luminosity;*/
 
 .hero-image img:hover {
   -webkit-transform: rotate(0deg);
